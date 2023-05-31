c79775a582445e77bb70924b87da3dd75bed3749
plavy <tinplavec@gmail.com>
2023-06-01 00:34:25 +0200

    fix oval box and selection distance

--- lab4/Oval.java
+++ lab4/Oval.java
@@ -25,20 +25,20 @@ public class Oval extends AbstractGraphicalObject {
     public double selectionDistance(Point mousePoint) {
         int centerX = getCenter().getX();
         int centerY = getCenter().getY();
-        double radiusX = getHotPoint(0).getX() - getHotPoint(1).getX();
-        double radiusY = getHotPoint(1).getY() - getHotPoint(0).getY();
+        double radiusX = Math.abs(getHotPoint(0).getX() - getHotPoint(1).getX());
+        double radiusY = Math.abs(getHotPoint(1).getY() - getHotPoint(0).getY());
 
         // Normalized distance to center
-        double deltaX = (mousePoint.getX() - centerX) / radiusX;
-        double deltaY = (mousePoint.getY() - centerY) / radiusY;
+        double deltaX = Math.abs((mousePoint.getX() - centerX) / radiusX);
+        double deltaY = Math.abs((mousePoint.getY() - centerY) / radiusY);
 
         double distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-        // TODO: Fix
         distance = distance - 1;
         if (distance < 0) {
             return 0;
         }
-        return distance * radiusY;
+        // Approximation
+        return distance * Math.min(radiusX, radiusY);
     }
 
     @Override
@@ -47,6 +47,14 @@ public class Oval extends AbstractGraphicalObject {
         int y = getHotPoint(0).getY() * 2 - getHotPoint(1).getY();
         int width = (getHotPoint(0).getX() - getHotPoint(1).getX()) * 2;
         int height = (getHotPoint(1).getY() - getHotPoint(0).getY()) * 2;
+        if (width < 0) {
+            width = -width;
+            x = x - width;
+        }
+        if (height < 0) {
+            height = -height;
+            y = y - height;
+        }
         return new Rectangle(x, y, width, height);
     }
 
@@ -71,7 +79,7 @@ public class Oval extends AbstractGraphicalObject {
         int a = getHotPoint(0).getX() - getHotPoint(1).getX();
         int b = getHotPoint(1).getY() - getHotPoint(0).getY();
         for (int i = 0; i < NUMBER_OF_POINTS / 2; i++){
-            double x = getHotPoint(1).getX() * 2 - getHotPoint(0).getX() + i * (getHotPoint(0).getX() - getHotPoint(1).getX()) / (NUMBER_OF_POINTS / 2 - 1) * 2;
+            double x = getHotPoint(1).getX() * 2 - getHotPoint(0).getX() + i * (getHotPoint(0).getX() - getHotPoint(1).getX()) * 2 / (NUMBER_OF_POINTS / 2 - 1);
             double tmp = Math.sqrt((1 - (Math.pow((x - p), 2) / Math.pow(a, 2))) * Math.pow(b, 2));
             double y1 = tmp + q;
             double y2 = - tmp + q;
